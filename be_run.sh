928c4fb87aa0f20e7c3d7b2d676f6e1c6ccf1b7d
jkbhatnagar <jatin.australia@gmail.com>
2025-08-10 14:07:53 +0000

    BE commit v0.6

--- be_run.sh
+++ be_run.sh
@@ -1,18 +1,18 @@
 # sudo chmod +x be_run.sh
 # ./be_run.sh
 
-kubectl apply -f k8s/secret.yaml        # only if using the file version
-kubectl apply -f k8s/configmap.yaml
-kubectl apply -f k8s/deployment.yaml
-kubectl apply -f k8s/service.yaml
+kubectl apply -f backend/k8s/secret.yaml        # only if using the file version
+kubectl apply -f backend/k8s/configmap.yaml
+kubectl apply -f backend/k8s/deployment.yaml
+kubectl apply -f backend/k8s/service.yaml
 
 kubectl get pods -l app=complaints-backend-deployment
 kubectl get svc complaints-backend-service
 
-kubectl logs deploy/complaints-backend-service -f
+kubectl logs deploy/complaints-backend-deployment -f
 
 # --------- Run in a seperate terminal ---------
-# kubectl port-forward svc/complaints-service 8080:80
+# kubectl port-forward svc/complaints-backend-service 8080:80
 # --------- Run in a seperate terminal ---------
 
 # then use http://localhost:8080
