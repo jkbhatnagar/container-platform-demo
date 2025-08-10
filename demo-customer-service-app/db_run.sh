4b2aeea676f26b2e4f197ac4219aaf375d4a6f71
jkbhatnagar <jatin.australia@gmail.com>
2025-08-10 09:58:26 +0000

    DB commit v0.5

--- demo-customer-service-app/db_run.sh
+++ demo-customer-service-app/db_run.sh
@@ -1,12 +1,15 @@
-# cd container-platform-demo/demo-customer-service-app/database/
+# git clone https://github.com/jkbhatnagar/container-platform-demo.git
+# cd container-platform-demo/demo-customer-service-app/
+# sudo chmod +x db_run.sh
+# ./db_run.sh
 
 kubectl create configmap postgres-initdb --from-file=./database/init.sql
 
 kubectl apply -f database/db_deployment.yaml
 
-kubectl apply -f .database/db_service.yaml
+kubectl apply -f database/db_service.yaml
 
-sleep 15
+sleep 5
 
 kubectl port-forward svc/postgres-service 5432:5432
 
